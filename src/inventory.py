f7a37734ed31202e177b9690a5ea0e788323c719
Ridonk <georgeddunbar@gmail.com>
2018-12-27 04:14:51 -0500

    started json implementation

--- src/inventory.py
+++ src/inventory.py
@@ -1,4 +1,5 @@
 from product import Product
+import json
 
 
 class Inventory:
@@ -9,3 +10,13 @@ class Inventory:
     def add_existing_product(self, product: Product):
         print(product.get_product_details())
         self.products.append(product)
+
+    def load_from_json(self, data_file: str):
+        data = json.loads(data_file)
+        print(data)
+        # TODO: Setup JSON processing
+
+    def save_to_json(self):
+        print("Saving to JSON...")
+        print(self.products)
+        # TODO: Setup JSON saving
